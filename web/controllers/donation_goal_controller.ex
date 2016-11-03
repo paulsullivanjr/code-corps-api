5b61d5f3ed7ac6a11c727abc311e016d0034bb04
Mackenzie Hicks <hicks.264@gmail.com>
2016-11-03 15:58:51 -0700

    Refactor Donation Goals Controller Tests with helpers

--- web/controllers/donation_goal_controller.ex
+++ web/controllers/donation_goal_controller.ex
@@ -2,11 +2,17 @@ defmodule CodeCorps.DonationGoalController do
   use CodeCorps.Web, :controller
   use JaResource
 
+  import CodeCorps.Helpers.Query, only: [id_filter: 2]
+
   alias CodeCorps.DonationGoal
 
   plug :load_and_authorize_changeset, model: DonationGoal, only: [:create]
   plug :load_and_authorize_resource, model: DonationGoal, only: [:update, :delete]
   plug JaResource
+
+  def filter(_conn, query, "id", id_list) do
+    query |> id_filter(id_list)
+  end
 
   def handle_create(_conn, attributes) do
     %DonationGoal{}
